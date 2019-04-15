4dd5e88622dcc250dceb0c2be4a88e36de3910f4
Karina Kozarova <karinakozarova1303@gmail.com>
2019-04-15 09:21:55 +0300

    playing with detecting goals

--- main.ino
+++ main.ino
@@ -1,13 +1,21 @@
 #include <NewPing.h> 
-const int TriggerPin = 11; 
-const int EchoPin = 12;
- 
+const int TriggerPin = 3;
+const int TriggerPin2 = 6;
+const int EchoPin2 = 5; 
+const int EchoPin = 4; 
 NewPing sonar(TriggerPin, EchoPin, 100); 
-void setup() { 
-	Serial.begin(9600); 
-} 
+NewPing sonar2(TriggerPin2, EchoPin2, 100); 
 
-void loop() { 
-	int cm = sonar.ping_cm(); 
-	Serial.println(cm); 
+void setup() { 
+ Serial.begin(9600); 
 } 
+void loop() { 
+ int cm = sonar.ping_cm(); 
+ int cm2 = sonar2.ping_cm();
+ Serial.println("first"); 
+ Serial.println(cm); 
+ Serial.println("second"); 
+ Serial.println(cm2); 
+ //Serial.println('second',cm2); 
+ delay(250); 
+}
