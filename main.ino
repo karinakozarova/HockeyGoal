5bacffe07bf7ce69be1358a43be3bb0c8113b5ce
Karina Kozarova <karinakozarova1303@gmail.com>
2019-04-17 18:48:44 +0300

    fix condition and distance

--- main.ino
+++ main.ino
@@ -7,32 +7,32 @@ const int rightSensorTriggerPin = 6;
 const int rightSensorEchoPin = 5;
 const int leftSensorEchoPin = 4;
 
-const int goalDistance = 5;
+const int goalDistance = 30;
 
 NewPing left_sensor(leftSensorTriggerPin, leftSensorEchoPin, 100);
 NewPing right_sensor(rightSensorTriggerPin, rightSensorEchoPin, 100);
 
 void setup() {
-	pinMode(ledPin,OUTPUT);
-	Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-	digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW);
 
-	int leftDistance = left_sensor.ping_cm();
-	int rightDistance = right_sensor.ping_cm();
+  int leftDistance = left_sensor.ping_cm();
+  int rightDistance = right_sensor.ping_cm();
 
-	Serial.print("Left sensor:");
-	Serial.println(leftDistance);
-	Serial.print("Right sensor:");
-	Serial.println(rightDistance);
+  Serial.print("Left sensor:");
+  Serial.println(leftDistance);
+  Serial.print("Right sensor:");
+  Serial.println(rightDistance);
 
-	if(leftDistance < goalDistance || rightDistance < goalDistance ){
-		digitalWrite(ledPin, HIGH);
-		delay(5000);
-		digitalWrite(ledPin, LOW);
-	}
+  if((leftDistance < goalDistance && leftDistance > 0) || (rightDistance < goalDistance && rightDistance > 0) ){
+    digitalWrite(ledPin, HIGH);
+    delay(5000);
+    digitalWrite(ledPin, LOW);
+  }
 
-	// delay(250);
+//  delay(250);
 }
